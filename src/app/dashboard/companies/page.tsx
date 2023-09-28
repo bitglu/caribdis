79a5ae0d848be8070d2544dd3e45c351584865e4
The Briant Carrillo <brianttd24.7@gmail.com>
2023-09-28 00:50:45 -0400

    agregamos env.local

--- src/app/dashboard/companies/page.tsx
+++ src/app/dashboard/companies/page.tsx
@@ -9,5 +9,13 @@ export default async function Account() {
     data: { session },
   } = await supabase.auth.getSession();
 
-  return <AccountForm session={session} />;
+  
+
+  return (
+    <>
+    <AccountForm session={session} /> 
+    <div>hola</div>
+
+    </>
+  );
 }
